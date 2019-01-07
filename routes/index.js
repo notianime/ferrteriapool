6ca191665b2096b4eaa2be0c946cd7a8137bb51f
notianime <i1028417@continental.edu.pe>
2019-01-07 11:23:59 -0500

    preparado

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Ferreteria Pool' });
+});
+router.get('/login', (req, res)=>{
+  res.render('login');
+});
+
+router.get('/tienda', (req, res)=>{
+  res.render('tienda');
+});
+
+router.get('/carrito', (req, res)=>{
+  res.render('carrito');
+});
+
+router.get('/nosotros', (req, res)=>{
+  res.render('nosotros');
+});
+
+router.get('/contacto', (req, res)=>{
+  res.render('contacto');
+});
+
+module.exports = router;
